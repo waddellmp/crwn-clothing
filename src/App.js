32c9bd4eb2f51f9b5b705b12586dd36d4cbfa6ab
Matthew Waddell <matthew.waddell@swaymedical.com>
2020-03-18 10:47:08 -0500

    Pushing changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import HomePage from './homepage.component';
+import HomePage from './components/homepage/homepage.component';
+import './App.scss';
 
 function App() {
     return (
